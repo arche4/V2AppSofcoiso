ca281fee8cb78e3a83616706111212e5da4c50c1
Yony Zuluaga <yonyandres1987@gmail.com>
2019-08-17 13:19:19 -0500

    se sube el logo del coiso

--- target/generated-sources/annotations/com/co/sofcoiso/modelo/Caso_.java
+++ target/generated-sources/annotations/com/co/sofcoiso/modelo/Caso_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-08-11T19:21:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-08-17T13:10:59")
 @StaticMetamodel(Caso.class)
 public class Caso_ { 
 
